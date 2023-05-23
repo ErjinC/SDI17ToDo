ae838735b63688038f0b478876654f19606122b4
ErjinC <erjinchoi@gmail.com>
2023-05-23 12:34:50 -0700

    Changed to checkboxes. Added deletion feature

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const form = document.getElementById('listform');
+var xoutListener;
+
 form.addEventListener('submit', () => {
   //Prevents submission
   event.preventDefault();
   //Adds input content into list
   var listContainer = document.querySelector('#listcontainer');
-  var listElement = document.createElement('li');
+
+  var listElement = document.createElement('label');
   var listContent = document.getElementById('listcontent').value;
   listElement.innerHTML = listContent;
-  listContainer.appendChild(listElement);
-  //Adds cross out and delete buttons
-  var xoutButton = document.createElement('button')
-  var deleteButton = document.createElement('button')
-  listElement.appendChild(xoutButton);
-  xoutButton.innerHTML = 'Cross me out!';
-  listElement.appendChild(deleteButton);
-  deleteButton.innerHTML = 'Delete me!';
+
+  var listCheckbox = document.createElement('input');
+  listCheckbox.setAttribute('type', 'checkbox');
+  listCheckbox.setAttribute('class', 'delete-this')
+
+  var listUnorderedContainer = document.createElement('li');
+
+  listUnorderedContainer.appendChild(listElement);
+  listUnorderedContainer.appendChild(listCheckbox);
+  listContainer.appendChild(listUnorderedContainer);
+  // listContainer.appendChild(document.createElement("br"));
 });
+
+function removeCheckedCheckboxes() {
+  var checked = document.querySelectorAll('.delete-this:checked');
+  checked.forEach((elem) => {
+    elem.parentElement.style.display = "none";
+  })
+}
