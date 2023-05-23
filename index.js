abe9affa3a32f38b8624a075b0b6191d4c997f1e
ErjinC <erjinchoi@gmail.com>
2023-05-23 08:38:32 -0700

    added working form submission and words show up

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hi!')
+const form = document.getElementById('listitem');
+
+form.addEventListener('submit', () => {
+  event.preventDefault();
+  var listContainer = document.querySelector('#list');
+  var listElement = document.createElement('p');
+  listElement.innerHTML = `contentcontent`;
+  listContainer.appendChild(listElement);
+
+  alert('pls work');
+
+});
+
