f43f4c4ec143c223871a68dc66c6a60fd0f6260c
ErjinC <erjinchoi@gmail.com>
2023-05-23 09:31:09 -0700

    Added cross out and delete buttons. Included comments

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const form = document.getElementById('listform');
-
 form.addEventListener('submit', () => {
+  //Prevents submission
   event.preventDefault();
+  //Adds input content into list
   var listContainer = document.querySelector('#listcontainer');
-  var listElement = document.createElement('p');
+  var listElement = document.createElement('li');
   var listContent = document.getElementById('listcontent').value;
   listElement.innerHTML = listContent;
   listContainer.appendChild(listElement);
+  //Adds cross out and delete buttons
+  var xoutButton = document.createElement('button')
+  var deleteButton = document.createElement('button')
+  listElement.appendChild(xoutButton);
+  xoutButton.innerHTML = 'Cross me out!';
+  listElement.appendChild(deleteButton);
+  deleteButton.innerHTML = 'Delete me!';
 });
-
